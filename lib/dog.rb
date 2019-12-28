135e954d8d121b03df75439998b244500a4c6e90
dwane richards <dwanerich@gmail.com>
2019-12-28 01:22:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name
+    @breed
+  end
+  
+  def name=(name)
+    @name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(breed)
+    @breed
+  end
+  
+  def breed
+    @breed
+  end
+  
+end
+
