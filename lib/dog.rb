d2eab2fa4d0eb5d873d7eadcd72e93c6bc832d8e
Damely Tineo <damely.tineo07@gmail.com>
2020-01-09 14:01:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,17 +4,9 @@ class Dog
     @name=name
     @breed=breed
   end
-  
-  def name=(name)
-    @name=name
-  end
-  
+
   def name 
     @name 
-  end
-  
-  def breed=(breed)
-    @breed=breed
   end
   
   def breed 
